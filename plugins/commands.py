6ee99a6d2f1d97248a45b3b6a7b688eef6bb5a2d
Ventrapragada Naga Venkata Sivasai Prajith <125337761+Jith2252@users.noreply.github.com>
2024-03-17 14:53:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -683,7 +683,7 @@ async def log_file(bot, message):
     except Exception as e:
         await message.reply(str(e))
 
-@Client.on_message(filters.command('delete') & filters.user(ADMINS))
+@Client.on_message(filters.command('d') & filters.user(ADMINS))
 async def delete(bot, message):
     """Delete file from database"""
     reply = message.reply_to_message
