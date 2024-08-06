4e81fe036f5758d1851774316614bc1b1cf5ed97
Ventrapragada Naga Venkata Sivasai Prajith <125337761+Jith2252@users.noreply.github.com>
2024-08-07 00:02:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,6 +57,7 @@ IS_STREAM = bool(STREAM_BIN and STREAM_URL)
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
+SPRT_CHNL = environ.get('SPRT_CHNL', 'https://t.me/nenmemeravthaa')
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gyanilinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '6aa57b9cf2e0491b7c8509e11f0acf57bcb16844')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
